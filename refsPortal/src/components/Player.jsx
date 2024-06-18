ffbe795798e9627cfc738fe628054352b9496d49
T-Rahul-prabhu-38 <trahulprabhu38@gmail.com>
2024-06-18 22:32:06 +0530

    project4

--- refsPortal/src/components/Player.jsx
+++ refsPortal/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState,useRef } from "react";
+
 export default function Player() {
+
+  const playerName = useRef('Anonymous');
+  const[username,setusername] = useState('');
+
+  function handleClick(){
+    setusername(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {username??'...who?'} !</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
